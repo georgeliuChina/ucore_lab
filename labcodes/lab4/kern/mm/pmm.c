fc27842786c7d012d75c18db502754a566b413d2
georgeliuChina <geogre_xl@163.com>
2015-04-25 11:13:33 +0800

    lab4 finished
    lab4 finished

--- labcodes/lab4/kern/mm/pmm.c
+++ labcodes/lab4/kern/mm/pmm.c
@@ -363,7 +363,7 @@ pmm_init(void) {
 // return vaule: the kernel virtual address of this pte
 pte_t *
 get_pte(pde_t *pgdir, uintptr_t la, bool create) {
-    /* LAB2 EXERCISE 2: YOUR CODE
+    /* LAB2 EXERCISE 2: P14206021
      *
      * If you need to visit a physical address, please use KADDR()
      * please read pmm.h for useful macros
@@ -396,6 +396,20 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
     }
     return NULL;          // (8) return page table entry
 #endif
+    pde_t *pde = &pgdir[PDX(la)];
+    // if there is no page
+    if ( (*pde & PTE_P) == 0 ) {
+        if (!create) return NULL;
+        struct Page *page;
+        // alloc a new page for it
+        page = alloc_page();
+        set_page_ref(page, 1);
+        uintptr_t pa = page2pa(page);
+        memset(KADDR(pa), 0, PGSIZE);
+        *pde = pa | PTE_U | PTE_W | PTE_P;
+    }
+    pte_t * pte_base = KADDR(PDE_ADDR(*pde));
+    return pte_base + PTX(la);
 }
 
 //get_page - get related Page struct for linear address la using PDT pgdir
@@ -416,7 +430,7 @@ get_page(pde_t *pgdir, uintptr_t la, pte_t **ptep_store) {
 //note: PT is changed, so the TLB need to be invalidate 
 static inline void
 page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
-    /* LAB2 EXERCISE 3: YOUR CODE
+    /* LAB2 EXERCISE 3: P14206021
      *
      * Please check if ptep is valid, and tlb must be manually updated if mapping is updated
      *
@@ -441,6 +455,13 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
                                   //(6) flush tlb
     }
 #endif
+    if (*ptep & PTE_P) {
+        struct Page *page = pte2page(*ptep);
+        // if no reference to page, free it
+        if (page_ref_dec(page) == 0) free_page(page);
+        tlb_invalidate(pgdir, la);
+    }
+    return;
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
--- labcodes/lab4/kern/mm/swap_fifo.c
+++ labcodes/lab4/kern/mm/swap_fifo.c
@@ -49,8 +49,11 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
  
     assert(entry != NULL && head != NULL);
     //record the page access situlation
-    /*LAB3 EXERCISE 2: YOUR CODE*/ 
+    /*LAB3 EXERCISE 2: P14206021*/ 
     //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
+
+    // Insert at back, because the list is a circle, we could just add it before the head.
+    list_add_before(head, entry);
     return 0;
 }
 /*
@@ -64,9 +67,16 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
          assert(head != NULL);
      assert(in_tick==0);
      /* Select the victim */
-     /*LAB3 EXERCISE 2: YOUR CODE*/ 
+     /*LAB3 EXERCISE 2: P14206021*/ 
      //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
      //(2)  set the addr of addr of this page to ptr_page
+
+     // The earliest page is just after the head
+     list_entry_t *le = list_next(head); 
+     assert(head != le);
+     struct Page *page = le2page(le, pra_page_link);
+     list_del(le);
+     *ptr_page = page;
      return 0;
 }
 
